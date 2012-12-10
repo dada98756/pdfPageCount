f707e744a78cab82343f9ed4233bfa2d1c8847b7
taomo117 <taomo117@gmail.com>
2012-12-09 20:42:09 -0500

    initial commit

---
+++ pdfPageCount.py
@@ -0,0 +1,18 @@
+from pyPdf import PdfFileReader
+import sys
+
+def main():
+	filename = sys.argv[1]
+	if (filename[-4:] != ".pdf"):
+		return "Error: Not a valid PDF file."
+	try:
+		fp = file(filename, 'rb')
+		pdf_f = PdfFileReader(fp)
+	except IOError as e:
+		return "ERROR: Unable to process file "+filename
+	except e:
+		return "ERROR: Unable to read PDF File"
+	return pdf_f.getNumPages()
+
+if __name__ == "__main__":
+    print main()
